1b58f22a14a4e7018de7088daf929eba13bf6b3f
Desmarres Sébastien <desmarres.sebastien@gmail.com>
2022-09-24 09:57:53 +0200

    MAJ DeploiementHerokuMysql

--- config/db.config.js
+++ config/db.config.js
@@ -6,13 +6,15 @@ const userName = process.env.DB_USERNAME || '';
 const password = process.env.DB_PASSWORD || '';
 const serverName = process.env.DB_SERVER_NAME || '';
 const database = process.env.DATABASE || '';
+const port = process.env.DB_PORT || '';
 
-/* connection à la base de donnée MongoDB*/
+/* connection à la base de donnée MYSQL*/
 const db = new Sequelize({
     database : database,
     username : userName,
     password : password,
     host: serverName,
+    port: port,
     dialect: 'mysql'
 });
 
